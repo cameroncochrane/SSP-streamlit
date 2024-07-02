2a4739cfe116c6cd58260ac89240e0deef1afda2
Cameron Cochrane <cochranecameron1@gmail.com>
2024-07-02 22:50:20 +0100

    Commit 3

--- ssp_app.py
+++ ssp_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 from streamlit_option_menu import option_menu
+import streamlit_shadcn_ui as ui
 
 #Custom modules:
 import chart_functions as cf
@@ -13,5 +14,11 @@ import chart_functions as cf
 def home():
     st.title("Supermarket Sales Data Analysis")
     st.write("This dashboard app features the full analysis of the supermarket sales data.")
+    st.write("Navigate the app using the pages menu on the left.")
+
+def shadcn_button():
+    trigger_button = ui.button(text='This is a button')
+    return trigger_button
 
 home()
+button = shadcn_button()
