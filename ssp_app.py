840ed333782c800249bc6f287d82731269e7ffbb
Cameron Cochrane <cochranecameron1@gmail.com>
2024-07-06 16:55:15 +0100

    13

--- ssp_app.py
+++ ssp_app.py
@@ -68,6 +68,27 @@ def gender_analysis():
 def member_analysis():
     st.title("Supermarket Sales Data Analysis: Member Analysis")
 
+    #Sales:
+    st.title(":red[Member Analysis: Sales]")
+    sales_graph, dataframe_sales = cf.plot_member_data(type='sales')
+
+    st.pyplot(fig=sales_graph,use_container_width=True)
+    st.dataframe(dataframe_sales,use_container_width=True)
+    
+    #Orders:
+    st.title(":red[Member Analysis: Orders]")
+    orders_graph, dataframe_orders = cf.plot_member_data(type='orders')
+
+    st.pyplot(fig=orders_graph,use_container_width=True)
+    st.dataframe(dataframe_orders,use_container_width=True)
+
+    #Sale per Order:
+    st.title(":red[Member Analysis: Average Sale per Order]")
+    aso_graph, dataframe_aso = cf.plot_member_data(type='sale_per_order')
+
+    st.pyplot(fig=aso_graph,use_container_width=True)
+    st.dataframe(dataframe_aso,use_container_width=True)
+
 
 #Sidebar:
 with st.sidebar:
